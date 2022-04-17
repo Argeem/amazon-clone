2f251cabf5ff72358a21e6244fb1990525e5605d
rgeem <sitthisakbannob@gmail.com>
2022-04-17 16:03:10 +0700

    create checkout page and route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import './App.css';
+import { BrowserRouter as Router, Routes ,Route} from 'react-router-dom'
 import Header from './components/Header'
 import Home from './components/Home'
+import Checkout from './components/Checkout'
+
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Home/>
-    </div>
+    <Router>
+      <div className="App">
+        <Header/>
+        <Routes>
+          <Route path="/" exact element={<Home/>}/> {/* Home */}
+          <Route path="/checkout" element={<Checkout/>}/> {/* Basket */}
+        </Routes>
+      </div>
+    </Router>
   );
+  // return(
+  //   <div className="App">
+  //     <Header/>
+  //     <Home/>
+  //   </div>
+  // );
 }
 
 export default App;
